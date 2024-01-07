388c514b187900b8807513ffa25c5013f1509ad7
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2024-01-07 17:14:36 +0400

    aside done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { StatisticComponent } from './components/statistic/statistic.component';
+import { StrategyComponent } from './components/strategy/strategy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AsideComponent,
+    UserInfoComponent,
+    StatisticComponent,
+    StrategyComponent
   ],
   imports: [
     BrowserModule,
