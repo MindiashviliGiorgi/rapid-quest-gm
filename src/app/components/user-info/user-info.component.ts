97e8ce5fa70add45ee8abbcd3f52bc50d46137f7
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2024-01-07 18:17:58 +0400

    user information done

--- src/app/components/user-info/user-info.component.ts
+++ src/app/components/user-info/user-info.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-info.component.scss']
 })
 export class UserInfoComponent {
+ 
+  userInfo : any = {
+    avatar : '/assets/images/user.jpg',
+    name : "Ned",
+    accountBalance : 19.892,
+    contributions : 4000,
+    total : 1.892,
+    transitions : [
+      {
+        date : '2020-08-25',
+        transferName : 'Withdrawal Transfer to Bank-XXX11'
+      },
+      {
+        date : '2020-07-18',
+        transferName : 'Withdrawal Transfer to Bank-XXX99'
+      },
+      {
+        date : '2020-07-14',
+        transferName : 'Withdrawal Transfer to Bank-XXX14'
+      }
+    ]
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }
